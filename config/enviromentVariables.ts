37dbe012f5cda387b638980878c785a02789afb5
juliocesardemoraes <jcmcf2012@gmail.com>
2022-05-03 10:39:06 -0300

    fix - refactor .env enviroment file

--- config/enviromentVariables.ts
+++ config/enviromentVariables.ts
@@ -1,6 +1,6 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
-export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_URI = process.env.MONGO_VERCEL_URI || process.env.MONGO_URI;
 
-export const MONGO_DB = process.env.MONGO_DB;
+export const MONGO_DB = process.env.MONGO_VERCEL_DB || process.env.MONGO_DB;
