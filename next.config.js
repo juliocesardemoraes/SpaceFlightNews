f2467aea59ee3b7b79320f2141218fcc6abe0ecd
juliocesardemoraes <jcmcf2012@gmail.com>
2022-05-03 09:11:42 -0300

    fix - removing env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
 };
 
 module.exports = nextConfig;
