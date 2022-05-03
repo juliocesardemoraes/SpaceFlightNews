26c7318e97276d63b91d1e5115b93339173e225e
juliocesardemoraes <jcmcf2012@gmail.com>
2022-05-03 10:17:26 -0300

    fix - removing console.log

--- util/connectMongo.ts
+++ util/connectMongo.ts
@@ -23,7 +23,6 @@ export async function dbConnect() {
       return;
     }
     const mongoUri: string | any = MONGO_URI;
-    console.log(mongoUri);
     connection = await mongoose.connect(mongoUri, {
       dbName: MONGO_DB,
     });
