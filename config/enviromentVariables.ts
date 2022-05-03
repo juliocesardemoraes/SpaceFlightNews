0e50f98eaeec65e86d570de16843e4cf04cbe9c2
juliocesardemoraes <jcmcf2012@gmail.com>
2022-05-02 22:16:38 -0300

    feat - changing from mongo connection to mongoose

--- config/enviromentVariables.ts
+++ config/enviromentVariables.ts
@@ -1,6 +1,6 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
-export const MONGO_URI = process.env.MONGO_URI || process.env.MONGODB_URI;
+export const MONGO_URI = process.env.MONGO_URI;
 
 export const MONGO_DB = process.env.MONGO_DB;
--- util/connectMongo.ts
+++ util/connectMongo.ts
@@ -1,10 +1,7 @@
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 import { MONGO_URI, MONGO_DB } from "../config/enviromentVariables";
 
-console.log(MONGO_DB);
-console.log(MONGO_URI);
-
-// Check for the mongo variables
+// Check if the mongo variables exist
 if (!MONGO_URI) {
   throw new Error("Define the MONGODB_URI environmental variable");
 }
@@ -13,32 +10,19 @@ if (!MONGO_DB) {
   throw new Error("Define the MONGODB_DB environmental variable");
 }
 
-let cachedClient = null;
-let cachedDb = null;
-
 /**
  * Connects to the database
- * @returns {MongoClient}: Database connection online
+ * @returns {mongoose}: Database connection online
  */
 
-export async function connectToDatabase() {
-  // Check if the data is cached. And loads from it
-  if (cachedClient && cachedDb) {
-    return {
-      client: cachedClient,
-      db: cachedDb,
-    };
+export async function dbConnect() {
+  try {
+    const mongoUri: string | any = MONGO_URI;
+    const connected = await mongoose.connect(mongoUri, {
+      dbName: MONGO_DB,
+    });
+    return connected;
+  } catch (error) {
+    throw new Error("Coudn't connect to the database" + error);
   }
-
-  const client = new MongoClient(MONGO_URI);
-  await client.connect();
-  const db = client.db(MONGO_DB);
-
-  cachedClient = client;
-  cachedDb = db;
-
-  return {
-    client: cachedClient,
-    db: cachedDb,
-  };
 }
